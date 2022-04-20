ab88f82ca47d74688ff497d0ee9e8a94eb25eea9
SebS <78222590+HackerEmaraks@users.noreply.github.com>
2022-04-20 18:18:20 +0100

    Main0.1.py
    Updated

---
+++ main.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+attempts = {}
+Loaded_Users = []
+
+print("HackerEmaraks' Password system 0.1")
+print("Booting Up..")
+sleep(2.7)
+print("Booted!")
+sleep(1.67)
+Username = input("What is yor Username? ")
+if Username in Loaded_Users:
+	  print("Username in 'Loaded_Users'. 
