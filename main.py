677a714b827c7638550fed0671811ab9d161992b
SebS <78222590+HackerEmaraks@users.noreply.github.com>
2022-04-21 07:39:18 +0100

    Main0.31.py
    Updated Answer

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ sleep(1.67)
 Username = input("What is yor Username? ")
 if Username in Loaded_Users:
     print("Username in 'Loaded_Users'. Please continue to Log In by pressing Enter.")
-	  
+    input("> ")
+    pass
+else:
+    pass
